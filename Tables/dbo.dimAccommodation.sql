1a7aa070afe66c2c8fe401cc0b0f257796869e3f
SkrytyZubr <66427481+SkrytyZubr@users.noreply.github.com>
2025-01-16 18:24:51 +0100

    v1.1 db added

--- Tables/dbo.dimAccommodation.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[dimAccommodation] (
-  [id_accommodation] [int] IDENTITY,
-  [Accommodation] [nvarchar](max) NULL,
-  [Accommodation_description] [nvarchar](max) NULL,
-  PRIMARY KEY CLUSTERED ([id_accommodation])
-)
-ON [PRIMARY]
-TEXTIMAGE_ON [PRIMARY]
-GO
