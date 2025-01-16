5053b3241beeca8edad72a4f2c6b9efd29b69925
SkrytyZubr <66427481+SkrytyZubr@users.noreply.github.com>
2025-01-16 18:18:42 +0100

    Delete Tables directory

--- Tables/dbo.dimResidency.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[dimResidency] (
-  [id_residency] [int] IDENTITY,
-  [residency_code] [nvarchar](max) NULL,
-  [residency_description] [nvarchar](max) NULL,
-  PRIMARY KEY CLUSTERED ([id_residency])
-)
-ON [PRIMARY]
-TEXTIMAGE_ON [PRIMARY]
-GO
--- Tables/dbo.dimUnit.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[dimUnit] (
-  [id_unit] [int] IDENTITY,
-  [unit] [nvarchar](max) NULL,
-  PRIMARY KEY CLUSTERED ([id_unit])
-)
-ON [PRIMARY]
-TEXTIMAGE_ON [PRIMARY]
-GO
